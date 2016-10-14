2dcf2ac294d4fe6f0642f993a5037f97b3da98e2
ashwinchidambaram1 <ashwinchidambaram1@gmail.com>
2016-10-14 02:31:39 -0400

    sampling with names - try 6

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
   	if (params.has_key?(:id) && params.has_key?(:var))
   		@id = params[:id]
   		@name = params[:var]
+  		@name2 = params[:name2]
+  		@name3 = params[:name3]
 
   	end
   end
